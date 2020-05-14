d0fb440041d9e9c53c9d3c71a2c50c6e9b814982
Michał Mikulski <m.mikulski@vp.pl>
2020-05-14 18:51:49 +0200

    Finished Zadanie14 with Resistor Calculator

---
+++ Zadanie14/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Zadanie14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbPasek1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbPasek2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbMnoznik = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbTolerancja = new System.Windows.Forms.ComboBox();
+            this.lblObliczenie = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbPasek1
+            // 
+            this.cbPasek1.FormattingEnabled = true;
+            this.cbPasek1.Items.AddRange(new object[] {
+            "Brązowy",
+            "Czerwony",
+            "Pomarańczowy",
+            "Żółty",
+            "Zielony",
+            "Niebieski",
+            "Fioletowy",
+            "Szary",
+            "Biały"});
+            this.cbPasek1.Location = new System.Drawing.Point(140, 23);
+            this.cbPasek1.Name = "cbPasek1";
+            this.cbPasek1.Size = new System.Drawing.Size(193, 21);
+            this.cbPasek1.TabIndex = 0;
+            this.cbPasek1.SelectedIndexChanged += new System.EventHandler(this.cbPasek1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(57, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pasek 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(57, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Pasek 2";
+            // 
+            // cbPasek2
+            // 
+            this.cbPasek2.FormattingEnabled = true;
+            this.cbPasek2.Items.AddRange(new object[] {
+            "Czarny",
+            "Brązowy",
+            "Czerwony",
+            "Pomarańczowy",
+            "Żółty",
+            "Zielony",
+            "Niebieski",
+            "Fioletowy",
+            "Szary",
+            "Biały"});
+            this.cbPasek2.Location = new System.Drawing.Point(140, 60);
+            this.cbPasek2.Name = "cbPasek2";
+            this.cbPasek2.Size = new System.Drawing.Size(193, 21);
+            this.cbPasek2.TabIndex = 3;
+            this.cbPasek2.SelectedIndexChanged += new System.EventHandler(this.cbPasek2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(52, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mnożnik";
+            // 
+            // cbMnoznik
+            // 
+            this.cbMnoznik.FormattingEnabled = true;
+            this.cbMnoznik.Items.AddRange(new object[] {
+            "Srebrny",
+            "Złoty",
+            "Czarny",
+            "Brązowy",
+            "Czerwony",
+            "Pomarańczowy",
+            "Żółty",
+            "Zielony",
+            "Niebieski",
+            "Fioletowy",
+            "Szary",
+            "Biały"});
+            this.cbMnoznik.Location = new System.Drawing.Point(140, 97);
+            this.cbMnoznik.Name = "cbMnoznik";
+            this.cbMnoznik.Size = new System.Drawing.Size(193, 21);
+            this.cbMnoznik.TabIndex = 5;
+            this.cbMnoznik.SelectedIndexChanged += new System.EventHandler(this.cbMnoznik_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(34, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tolerancja";
+            // 
+            // cbTolerancja
+            // 
+            this.cbTolerancja.FormattingEnabled = true;
+            this.cbTolerancja.Items.AddRange(new object[] {
+            "Brak",
+            "Srebrny",
+            "Złoty",
+            "Brązowy",
+            "Czerwony",
+            "Zielony",
+            "Niebieski",
+            "Fioletowy",
+            "Szary"});
+            this.cbTolerancja.Location = new System.Drawing.Point(140, 134);
+            this.cbTolerancja.Name = "cbTolerancja";
+            this.cbTolerancja.Size = new System.Drawing.Size(193, 21);
+            this.cbTolerancja.TabIndex = 7;
+            this.cbTolerancja.SelectedIndexChanged += new System.EventHandler(this.cbTolerancja_SelectedIndexChanged);
+            // 
+            // lblObliczenie
+            // 
+            this.lblObliczenie.AutoSize = true;
+            this.lblObliczenie.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblObliczenie.Location = new System.Drawing.Point(136, 184);
+            this.lblObliczenie.Name = "lblObliczenie";
+            this.lblObliczenie.Size = new System.Drawing.Size(65, 24);
+            this.lblObliczenie.TabIndex = 9;
+            this.lblObliczenie.Text = "0Ω 0%";
+            this.lblObliczenie.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 302);
+            this.Controls.Add(this.lblObliczenie);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbTolerancja);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbMnoznik);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbPasek2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbPasek1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kalkulator opornika 4-paskowego";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbPasek1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbPasek2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbMnoznik;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbTolerancja;
+        private System.Windows.Forms.Label lblObliczenie;
+    }
+}
+
---
+++ Zadanie14/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        int pasek1Opor = 0;
+        int pasek2Opor = 0;
+        double mnoznik = 1;
+        double tolerancja = 0;
+
+        private void cbPasek1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cbPasek1.SelectedIndex > -1)
+            {
+                switch (cbPasek1.SelectedItem)
+                {
+                    case "Czarny":
+                        pasek1Opor = 0;
+                        cbPasek1.BackColor = Color.Black;
+                        cbPasek1.ForeColor = Color.White;
+                        break;
+                    case "Brązowy":
+                        pasek1Opor = 1;
+                        cbPasek1.BackColor = Color.Brown;
+                        cbPasek1.ForeColor = Color.White;
+
+                        break;
+                    case "Czerwony":
+                        pasek1Opor = 2;
+                        cbPasek1.BackColor = Color.Red;
+                        cbPasek1.ForeColor = Color.White;
+
+                        break;
+                    case "Pomarańczowy":
+                        pasek1Opor = 3;
+                        cbPasek1.BackColor = Color.Orange;
+                        cbPasek1.ForeColor = Color.White;
+
+                        break;
+                    case "Żółty":
+                        pasek1Opor = 4;
+                        cbPasek1.BackColor = Color.Yellow;
+                        cbPasek1.ForeColor = Color.Black;
+
+                        break;
+                    case "Zielony":
+                        pasek1Opor = 5;
+                        cbPasek1.BackColor = Color.Green;
+                        cbPasek1.ForeColor = Color.White;
+
+                        break;
+                    case "Niebieski":
+                        pasek1Opor = 6;
+                        cbPasek1.BackColor = Color.Blue;
+                        cbPasek1.ForeColor = Color.White;
+
+                        break;
+                    case "Fioletowy":
+                        pasek1Opor = 7;
+                        cbPasek1.BackColor = Color.Violet;
+                        cbPasek1.ForeColor = Color.White;
+
+                        break;
+                    case "Szary":
+                        pasek1Opor = 8;
+                        cbPasek1.BackColor = Color.LightGray;
+                        cbPasek1.ForeColor = Color.Black;
+
+                        break;
+                    case "Biały":
+                        pasek1Opor = 9;
+                        cbPasek1.BackColor = Color.White;
+                        cbPasek1.ForeColor = Color.Black;
+
+                        break;
+                    default:
+                        pasek1Opor = 0;
+                        cbPasek1.BackColor = Color.White;
+                        cbPasek1.ForeColor = Color.Black;
+
+                        break;
+
+                }
+
+                lblObliczenie.Text = string.Format("{0}Ω {1}%", (pasek1Opor * 10 + pasek2Opor) * mnoznik, tolerancja);
+
+            }
+        }
+
+        private void cbPasek2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbPasek2.SelectedIndex > -1)
+            {
+                switch (cbPasek2.SelectedItem)
+                {
+                    case "Czarny":
+                        pasek2Opor = 0;
+                        cbPasek2.BackColor = Color.Black;
+                        cbPasek2.ForeColor = Color.White;
+                        break;
+                    case "Brązowy":
+                        pasek2Opor = 1;
+                        cbPasek2.BackColor = Color.Brown;
+                        cbPasek2.ForeColor = Color.White;
+                        break;
+                    case "Czerwony":
+                        pasek2Opor = 2;
+                        cbPasek2.BackColor = Color.Red;
+                        cbPasek2.ForeColor = Color.White;
+                        break;
+                    case "Pomarańczowy":
+                        pasek2Opor = 3;
+                        cbPasek2.BackColor = Color.Orange;
+                        cbPasek2.ForeColor = Color.White;
+
+                        break;
+                    case "Żółty":
+                        pasek2Opor = 4;
+                        cbPasek2.BackColor = Color.Yellow;
+                        cbPasek2.ForeColor = Color.Black;
+                        break;
+                    case "Zielony":
+                        pasek2Opor = 5;
+                        cbPasek2.BackColor = Color.Green;
+                        cbPasek2.ForeColor = Color.White;
+                        break;
+                    case "Niebieski":
+                        pasek2Opor = 6;
+                        cbPasek2.BackColor = Color.Blue;
+                        cbPasek2.ForeColor = Color.White;
+                        break;
+                    case "Fioletowy":
+                        pasek2Opor = 7;
+                        cbPasek2.BackColor = Color.Violet;
+                        cbPasek2.ForeColor = Color.White;
+                        break;
+                    case "Szary":
+                        pasek2Opor = 8;
+                        cbPasek2.BackColor = Color.LightGray;
+                        cbPasek2.ForeColor = Color.Black;
+                        break;
+                    case "Biały":
+                        pasek2Opor = 9;
+                        cbPasek2.BackColor = Color.White;
+                        cbPasek2.ForeColor = Color.Black;
+                        break;
+                    default:
+                        pasek2Opor = 0;
+                        cbPasek2.BackColor = Color.White;
+                        cbPasek2.ForeColor = Color.Black;
+                        break;
+
+                }
+
+                lblObliczenie.Text = string.Format("{0}Ω {1}%", (pasek1Opor * 10 + pasek2Opor) * mnoznik, tolerancja);
+
+            }
+        }
+
+        private void cbMnoznik_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbMnoznik.SelectedIndex > -1)
+            {
+                switch (cbMnoznik.SelectedItem)
+                {
+                    case "Srebrny":
+                        mnoznik = 0.01;
+                        cbMnoznik.BackColor = Color.LightGray;
+                        cbMnoznik.ForeColor = Color.Black;
+                        break;
+                    case "Złoty":
+                        mnoznik = 0.1;
+                        cbMnoznik.BackColor = Color.Gold;
+                        cbMnoznik.ForeColor = Color.Black;
+                        break;
+                    case "Czarny":
+                        mnoznik = 1;
+                        cbMnoznik.BackColor = Color.Black;
+                        cbMnoznik.ForeColor = Color.White;
+                        break;
+                    case "Brązowy":
+                        mnoznik = 10;
+                        cbMnoznik.BackColor = Color.Brown;
+                        cbMnoznik.ForeColor = Color.White;
+
+                        break;
+                    case "Czerwony":
+                        mnoznik = 100;
+                        cbMnoznik.BackColor = Color.Red;
+                        cbMnoznik.ForeColor = Color.White;
+                        break;
+                    case "Pomarańczowy":
+                        mnoznik = 1000;
+                        cbMnoznik.BackColor = Color.Orange;
+                        cbMnoznik.ForeColor = Color.White;
+
+                        break;
+                    case "Żółty":
+                        mnoznik = 10000;
+                        cbMnoznik.BackColor = Color.Yellow;
+                        cbMnoznik.ForeColor = Color.Black;
+                        break;
+                    case "Zielony":
+                        mnoznik = 100000;
+                        cbMnoznik.BackColor = Color.Green;
+                        cbMnoznik.ForeColor = Color.White;
+                        break;
+                    case "Niebieski":
+                        mnoznik = 1000000;
+                        cbMnoznik.BackColor = Color.Blue;
+                        cbMnoznik.ForeColor = Color.White;
+                        break;
+                    case "Fioletowy":
+                        mnoznik = 10000000;
+                        cbMnoznik.BackColor = Color.Violet;
+                        cbMnoznik.ForeColor = Color.White;
+                        break;
+                    case "Szary":
+                        mnoznik = 100000000;
+                        cbMnoznik.BackColor = Color.LightGray;
+                        cbMnoznik.ForeColor = Color.Black;
+                        break;
+                    case "Biały":
+                        mnoznik = 1000000000;
+                        cbMnoznik.BackColor = Color.White;
+                        cbMnoznik.ForeColor = Color.Black;
+                        break;
+                    default:
+                        mnoznik = 0;
+                        cbMnoznik.BackColor = Color.White;
+                        cbMnoznik.ForeColor = Color.Black;
+                        break;
+
+                }
+
+                lblObliczenie.Text = string.Format("{0}Ω {1}%", (pasek1Opor * 10 + pasek2Opor) * mnoznik, tolerancja);
+
+            }
+
+        }
+
+        private void cbTolerancja_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbTolerancja.SelectedIndex > -1)
+            {
+                switch (cbTolerancja.SelectedItem)
+                {
+                    case "Brak":
+                        tolerancja = 20;
+                        cbTolerancja.BackColor = Color.White;
+                        cbTolerancja.ForeColor = Color.Black;
+                        break;
+                    case "Srebrny":
+                        tolerancja = 10;
+                        cbTolerancja.BackColor = Color.LightGray;
+                        cbTolerancja.ForeColor = Color.Black;
+                        break;
+                    case "Złoty":
+                        tolerancja = 5;
+                        cbTolerancja.BackColor = Color.Gold;
+                        cbTolerancja.ForeColor = Color.Black;
+                        break;
+                    case "Brązowy":
+                        tolerancja = 1;
+                        cbTolerancja.BackColor = Color.Brown;
+                        cbTolerancja.ForeColor = Color.White;
+                        break;
+                    case "Czerwony":
+                        tolerancja = 2;
+                        cbTolerancja.BackColor = Color.Red;
+                        cbTolerancja.ForeColor = Color.White;
+                        break;
+                    case "Zielony":
+                        tolerancja = 0.5;
+                        cbTolerancja.BackColor = Color.Green;
+                        cbTolerancja.ForeColor = Color.White;
+                        break;
+                    case "Niebieski":
+                        tolerancja = 0.25;
+                        cbTolerancja.BackColor = Color.Blue;
+                        cbTolerancja.ForeColor = Color.White;
+                        break;
+                    case "Fioletowy":
+                        tolerancja = 0.1;
+                        cbTolerancja.BackColor = Color.Violet;
+                        cbTolerancja.ForeColor = Color.White;
+                        break;
+                    case "Szary":
+                        tolerancja = 0.05;
+                        cbTolerancja.BackColor = Color.LightGray;
+                        cbTolerancja.ForeColor = Color.Black;
+                        break;
+                    default:
+                        tolerancja = 0;
+                        cbTolerancja.BackColor = Color.White;
+                        cbTolerancja.ForeColor = Color.Black;
+                        break;
+
+                }
+
+                lblObliczenie.Text = string.Format("{0}Ω {1}%", (pasek1Opor * 10 + pasek2Opor) * mnoznik, tolerancja);
+
+            }
+
+        }
+    }
+}
