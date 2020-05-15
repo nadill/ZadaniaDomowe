2bdcc14b811b196412220cf59f458b3eec016aa8
Michał Mikulski <m.mikulski@vp.pl>
2020-05-15 23:50:32 +0200

    Finished Zadanie16 with Units calculator

---
+++ Zadanie16/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Zadanie16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbValue = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnResetList = new System.Windows.Forms.Button();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbValue
+            // 
+            this.tbValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbValue.Location = new System.Drawing.Point(68, 46);
+            this.tbValue.Multiline = true;
+            this.tbValue.Name = "tbValue";
+            this.tbValue.Size = new System.Drawing.Size(269, 42);
+            this.tbValue.TabIndex = 0;
+            this.tbValue.Text = "0";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 29;
+            this.listBox1.Items.AddRange(new object[] {
+            "kg -> funty",
+            "funty -> kg",
+            "C -> F",
+            "F -> C",
+            "km/h -> kts",
+            "kts -> km/h"});
+            this.listBox1.Location = new System.Drawing.Point(68, 115);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(269, 178);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnResetList
+            // 
+            this.btnResetList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnResetList.Location = new System.Drawing.Point(68, 317);
+            this.btnResetList.Name = "btnResetList";
+            this.btnResetList.Size = new System.Drawing.Size(269, 55);
+            this.btnResetList.TabIndex = 2;
+            this.btnResetList.Text = "Wyzeruj wybór z listy";
+            this.btnResetList.UseVisualStyleBackColor = true;
+            this.btnResetList.Click += new System.EventHandler(this.btnResetList_Click);
+            // 
+            // tbResult
+            // 
+            this.tbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tbResult.Location = new System.Drawing.Point(375, 46);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
+            this.tbResult.Size = new System.Drawing.Size(148, 42);
+            this.tbResult.TabIndex = 4;
+            this.tbResult.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 465);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.btnResetList);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.tbValue);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbValue;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnResetList;
+        private System.Windows.Forms.TextBox tbResult;
+    }
+}
+
---
+++ Zadanie16/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                double value = Convert.ToDouble(tbValue.Text);
+                if (listBox1.SelectedIndex > -1)
+                {
+                    if (tbValue.Text == "") tbValue.Text = "0";
+
+                    switch (listBox1.SelectedItem)
+                    {
+                        case "kg -> funty":
+                            if (value < 0)
+                            {
+                                value *= -1;
+                                tbValue.Text = value.ToString();
+                            }
+                            value *= 2.2046;
+                                tbResult.Text = string.Format("{0:0.00}funt", value);
+                                break;
+                        case "funty -> kg":
+                            if (value < 0)
+                            {
+                                value *= -1;
+                                tbValue.Text = value.ToString();
+                            }
+                            value /= 2.2046;
+                                tbResult.Text = string.Format("{0:0.00}kg", value);
+                                break;
+                        case "C -> F":
+                            value = value*1.8 + 32;
+                                tbResult.Text = string.Format("{0:0.00}F", value);
+                                break;
+                        case "F -> C":
+                            value = (value-32)/1.8;
+                                tbResult.Text = string.Format("{0:0.00}C", value);
+                                break;
+                        case "km/h -> kts":
+                            if (value < 0)
+                            {
+                                value *= -1;
+                                tbValue.Text = value.ToString();
+                            }
+                            value *= 0.62;
+                                tbResult.Text = string.Format("{0:0.00}kts", value);
+                                break;
+                        case "kts -> km/h":
+                            if (value < 0)
+                            {
+                                value *= -1;
+                                tbValue.Text = value.ToString();
+                            }
+                            value *= 1.61;
+                                tbResult.Text = string.Format("{0:0.00}km/h", value);
+                                break;
+                    
+                    }
+                }
+                else
+                {
+                    tbResult.Text = "0";
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void btnResetList_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectedIndex = -1;
+        }
+    }
+}
