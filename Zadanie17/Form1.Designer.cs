f53f75e59df802707274c8369917ba63d9f0851a
Michał Mikulski <m.mikulski@vp.pl>
2020-05-17 13:32:35 +0200

    Finished Zadanie17 with traveling panel

---
+++ Zadanie17/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Zadanie17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 150;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(600, 81);
+            this.panel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(100, 100);
+            this.panel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 461);
+            this.Controls.Add(this.panel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel;
+    }
+}
+
---
+++ Zadanie17/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+        int posX;
+        int posY;
+        int maxX;
+        int maxY;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form form = (Form)sender;
+            maxX = form.Size.Width;
+            maxY = form.Size.Height;
+            posX = panel.Location.X;
+            posY = panel.Location.Y;
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            // Nadanie panelowi nowego położenia XY
+            panel.Location = GeneratePoint();
+            // Nadanie panelowi nowego koloru
+            panel.BackColor = GenerateColor();
+        }
+
+        private Point GeneratePoint()
+        {
+            // Generowanie połóżenia X
+            posX += random.Next(-75, 75);
+            if (posX < 0)
+            {
+                posX = 0;
+            }
+            else if (posX > maxX - panel.Width)
+            {
+                posX = maxX - panel.Width;
+            }
+            // generowanie położenia Y
+            posY += random.Next(-75, 75);
+            if (posY < 0)
+            {
+                posY = 0;
+            }
+            else if (posY > maxY - panel.Height)
+            {
+                posY = maxY - panel.Height;
+            }
+
+            Point point = new Point(posX, posY);
+            return point;
+        }
+
+        private Color GenerateColor()
+        {
+            byte r = (byte)random.Next(0, 255);
+            byte g = (byte)random.Next(0, 255);
+            byte b = (byte)random.Next(0, 255);
+
+            Color color = new Color();
+            color = Color.FromArgb(r, g, b);
+
+            return color;
+        }
+    }
+}
